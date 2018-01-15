553db657e8f05a9c2620cc735e1d4905fc6b3b66
cqkisyouq <874496466@qq.com>
2018-01-15 19:37:53 +0800

    使 mvc 支持 把 view 放到任意目录

---
+++ 后台/Infrastructure/Extensions/MVCExtension/SourcePoint.Infrastructure.Extensions.MVCExtension/RazorProjectConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SourcePoint.Infrastructure.Extensions.MVCExtension
+{
+    public interface IRazorProjectConfiguration
+    {
+        string[] Paths { get; set; }
+    }
+    public class RazorProjectConfiguration:IRazorProjectConfiguration
+    {
+        public string[] Paths { get; set; } = new string[1];
+        public RazorProjectConfiguration() { }
+        public RazorProjectConfiguration(params string[] paths)
+        {
+            if (paths.Length > 0)
+            {
+                Paths = paths;
+            }
+        }
+    }
+}
