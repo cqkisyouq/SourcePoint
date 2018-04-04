0ed6bd58432f9ad3ebc93cb84a19effd49f56551
cqkisyouq <874496466@qq.com>
2018-04-04 18:26:51 +0800

    视图预编译示例

---
+++ 后台/samples/MvcPrecompilation/View2/Controllers/AppController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcPrecompilation.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
