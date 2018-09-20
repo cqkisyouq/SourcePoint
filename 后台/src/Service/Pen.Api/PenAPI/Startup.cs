ed81efe1d3cb5d1d2a453a23d237a62e011d5c8c
cqkisyouq <874496466@qq.com>
2018-09-20 17:50:49 +0800

    静态文件访问设置

--- 后台/src/Service/Pen.Api/PenAPI/Startup.cs
+++ 后台/src/Service/Pen.Api/PenAPI/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SourcePoint.Infrastructure.Extensions.SwaggerExtension;
@@ -33,15 +34,43 @@ namespace PenAPI
             });
         }
 
+        private static string IndexPath = "./wwwroot/index.html";
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            
+
+            #region 支持静态文件 设置网站默认打开 index.html 404重定向到 index.html
+            DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+            // defaultFilesOptions.RequestPath = "/wwwroot/dist";
+            defaultFilesOptions.DefaultFileNames.Clear();
+            //当访问网站域名时  默认请求下面指定的文件
+            defaultFilesOptions.DefaultFileNames.Add("index.html");
+            app.UseDefaultFiles(defaultFilesOptions);
+            app.UseStaticFiles();
+
+            //对 400 - 499 状态处理 这里为了支持vue前端只处理 404 状态
+            app.UseStatusCodePages(async status =>
+            {
+                if (status.HttpContext.Response.StatusCode == StatusCodes.Status404NotFound)
+                {
+                    if (File.Exists(IndexPath))
+                    {
+                        var context = File.ReadAllText(IndexPath);
+                        status.HttpContext.Response.StatusCode = StatusCodes.Status200OK;
+                        await status.HttpContext.Response.WriteAsync(context);
+                        return;
+                    }
+                }
+                await status.Next(status.HttpContext);
+            });
+
+            #endregion
+
             if (Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            app.UseStaticFiles();
+
             app.UseMvc();
             app.UseSwaggerDocument("v1");
         }
